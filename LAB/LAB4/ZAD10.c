fde60c196a9fd824884409e13276e189b1fb94d9
Luka Stošić <158442187+Stosic7@users.noreply.github.com>
2024-05-04 23:27:28 +0200

    Update ZAD10.c

--- LAB/LAB4/ZAD10.c
+++ LAB/LAB4/ZAD10.c
@@ -29,23 +29,26 @@ int main() {
 
     // Modifikacija matrice
     for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            if (j > i) {
-                int temp = A[i][N - 1];
-                for (int l = N - 1; l > i; l--) {
-                    A[i][l] = A[i][l - 1];
-                }
-                A[i][i] = temp;
+        for (int j = i+1; j < N; j++) { // Počinjemo od i+1 da preskočimo dijagonalu
+            if (j - i <= k) {
+                int temp = A[i][j];
+                A[i][j] = A[i][j - k]; // Postoji šansa da ima bagova, nisam dovoljno plaćen da debuggujem :)
+                A[i][j - k] = temp;
             }
         }
     }
 
     // Pronalaženje maksimalnog elementa ispod glavne dijagonale
     int maxElem = A[1][0];
-    for (int i = 2; i < N; i++) {
-        for (int j = 0; j < i; j++) {
-            if (A[i][j] > maxElem) {
-                maxElem = A[i][j];
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            if (i + j > N-1) {
+                if (A[i][j] > maxElem)
+                {
+
+                    maxElem = A[i][j]; // Fixovano
+
+                }
             }
         }
     }
@@ -57,7 +60,7 @@ int main() {
         for (int j = 0; j < N; j++) {
             printf("%d ", A[i][j]);
         }
-        printf("\n");
+        printf("\n");   
     }
 
     // Ispis maksimalnog elementa ispod glavne dijagonale
