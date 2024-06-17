f3c9d61a5307d09a6f0158d6b89f398cd4114ffa
Luka Stošić <158442187+Stosic7@users.noreply.github.com>
2024-06-18 00:32:34 +0200

    Update ZAD1.c

--- BLANKETI/II KOLOKVIJUM 2024/TERMIN 1/GRUPA A/ZAD1.c
+++ BLANKETI/II KOLOKVIJUM 2024/TERMIN 1/GRUPA A/ZAD1.c
@@ -1,94 +1,88 @@
 #include <stdio.h>
 
-int main()
-{
+void printMatrix(int matrix[15][15], int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (j != 0) {
+                printf(" | ");
+            }
+            printf("%d", matrix[i][j]);
+        }
+        printf("\n");
+    }
+}
 
-	int i, j, pom = -1, a[30], b[15][15], n, k, l, p;
+int main() {
 
-	FILE* file = fopen("matrica.txt", "r");
+    int i, j, pom = -1, a[30], b[15][15], n, k, l, p;
 
-	if (file == NULL) {
-		printf("Greska pri otvaranju fajla.\n");
-		return 1;
-	}
+    FILE* file = fopen("matrica.txt", "r");
 
-	fscanf(file, "%d", &n);
-	printf("\n");
+    if (file == NULL) {
+        printf("Greska pri otvaranju fajla.\n");
+        return 1;
+    }
 
-	for (i = 0; i < n; i++) {
-		for (j = 0; j < n; j++) {
-			fscanf(file, "%d", &b[i][j]);
-		}
-	}
-	fclose(file);
+    fscanf(file, "%d", &n);
+    printf("\n");
 
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            fscanf(file, "%d", &b[i][j]);
+        }
+    }
+    fclose(file);
 
-	for (i = 0; i < n; i++)
-		a[i] = b[i][0];
-	k = n;
-	for (j = 1; j < n; j++)
-	{
-		a[k] = b[0][j];
-		k++;
-	}
-	p = k;
-	for (i = 1; i < n; i++)
-	{
-		a[p] = b[i][n - 1];
-		p++;
-	}
+    for (i = 0; i < n; i++)
+        a[i] = b[i][0];
+    k = n;
+    for (j = 1; j < n; j++) {
+        a[k] = b[0][j];
+        k++;
+    }
+    p = k;
+    for (i = 1; i < n; i++) {
+        a[p] = b[i][n - 1];
+        p++;
+    }
 
-	l = p;
-	for (j = 1; j < n - 1; j++)
-	{
-		a[l] = b[n - 1][j];
-		l++;
-	}
+    l = p;
+    for (j = 1; j < n - 1; j++) {
+        a[l] = b[n - 1][j];
+        l++;
+    }
 
-	for (int i = 0; i < l - 1; i++) {
-		for (int j = 0; j < l - i - 1; j++) {
-			if (a[j] > a[j + 1]) {
-				int temp = a[j];
-				a[j] = a[j + 1];
-				a[j + 1] = temp;
-				pom = temp;
-			}
-		}
-	}
+    for (int i = 0; i < l - 1; i++) {
+        for (int j = 0; j < l - i - 1; j++) {
+            if (a[j] > a[j + 1]) {
+                int temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
+                pom = temp;
+            }
+        }
+    }
 
-	for (i = 0; i < n; i++)
-		b[i][0] = a[n - i - 1];
-	k = n;
-	for (j = 1; j < n; j++)
-	{
-		b[0][j] = a[k];
-		k++;
-	}
-	p = k;
-	for (i = 1; i < n; i++)
-	{
-		b[i][n - 1] = a[p];
-		p++;
-	}
+    for (i = 0; i < n; i++)
+        b[i][0] = a[n - i - 1];
+    k = n;
+    for (j = 1; j < n; j++) {
+        b[0][j] = a[k];
+        k++;
+    }
+    p = k;
+    for (i = 1; i < n; i++) {
+        b[i][n - 1] = a[p];
+        p++;
+    }
 
-	l = p;
-	for (j = n - 2; j > 0; j--)
-	{
-		b[n - 1][j] = a[l];
-		l++;
-	}
+    l = p;
+    for (j = n - 2; j > 0; j--) {
+        b[n - 1][j] = a[l];
+        l++;
+    }
 
-	for (int i = 0; i < n; i++)
-	{
+    printMatrix(b, n);
 
-		for (j = 0; j < n; j++)
-		{
-
-			printf("%d ", b[i][j]);
-
-		}
-		printf("\n");
-
-	}
-
+    return 0;
 }
