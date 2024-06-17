79f0b2b782d369085c00209a99892437eb8336a3
Luka Stošić <158442187+Stosic7@users.noreply.github.com>
2024-06-18 00:37:53 +0200

    Update ZAD1.c

--- BLANKETI/II KOLOKVIJUM 2024/TERMIN 2/GRUPA B/ZAD1.c
+++ BLANKETI/II KOLOKVIJUM 2024/TERMIN 2/GRUPA B/ZAD1.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 #include <string.h>
-void main()
-{
+
+void printMatrix(int matrix[10][10], int n) {
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (j != 0) {
+				printf(" | ");
+			}
+			printf("%d", matrix[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+int main() {
 	int b[10][10], n, a[30], i, j, k = 0, pom;
 	FILE* f = fopen("matrica.txt", "r");
 	fscanf(f, "%d", &n);
@@ -15,12 +27,9 @@ void main()
 		a[k++] = b[i][i];
 	for (i = n - 2; i >= 0; i--)
 		a[k++] = b[i][n - 1];
-	for (i = 0; i < k - 1; i++)
-	{
-		for (j = i + 1; j < k; j++)
-		{
-			if (a[i] > a[j])
-			{
+	for (i = 0; i < k - 1; i++) {
+		for (j = i + 1; j < k; j++) {
+			if (a[i] > a[j]) {
 				pom = a[i];
 				a[i] = a[j];
 				a[j] = pom;
@@ -34,10 +43,8 @@ void main()
 		b[i][i] = a[k++];
 	for (i = n - 2; i >= 0; i--)
 		b[i][n - 1] = a[k++];
-	for (i = 0; i < n; i++)
-	{
-		for (j = 0; j < n; j++)
-			printf("%d ", b[i][j]);
-		printf("\n");
-	}
+
+	printMatrix(b, n);
+
+	return 0;
 }
