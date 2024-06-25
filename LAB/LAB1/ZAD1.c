1e8884e0ff72a54b983340397cb0af22480575f3
Luka Stošić <158442187+Stosic7@users.noreply.github.com>
2024-06-25 18:08:48 +0200

    Update ZAD1.c

--- LAB/LAB1/ZAD1.c
+++ LAB/LAB1/ZAD1.c
@@ -29,13 +29,13 @@ int main() {
 }
 
 // ISPRAVKE:
--Dodato #include <stdio.h> na početak programa jer je to neophodno za funkcije poput printf i scanf.
+/*-Dodato #include <stdio.h> na početak programa jer je to neophodno za funkcije poput printf i scanf.
 -Promenio void main() u int main() jer funkcija main treba da vraća int.
 -Ispravio mala slova j u for petlji u for (J = F; J <= F + i; J++) jer su promenljive u C-u case-sensitive.
--Dodato return 0; na kraju funkcije main kako bi se ispravno završio program.
+-Dodato return 0; na kraju funkcije main kako bi se ispravno završio program.*/
 
 // STA KOD RADI?
 
-Ovaj program korisniku omogućava da unese pozitivan ceo broj N, a zatim računa faktorijel tog broja (označen sa F) 
+/*Ovaj program korisniku omogućava da unese pozitivan ceo broj N, a zatim računa faktorijel tog broja (označen sa F) 
 i zatim sumu brojeva od F do F+N (označenu sa S). Na kraju programa se ispisuje vrednost S 
-i proverava se da li je ta vrednost paran broj ili ne, pri čemu se ispisuje odgovarajuća poruka.
+i proverava se da li je ta vrednost paran broj ili ne, pri čemu se ispisuje odgovarajuća poruka.*/
